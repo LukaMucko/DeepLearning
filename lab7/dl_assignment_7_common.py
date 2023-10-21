fe9dcf3d4c427c3adc00629b90c8cf405dde63e4
Luca Poli <poli.luca.marconi@gmail.com>
2023-10-21 11:13:50 +0200

    created networks form the paper

--- lab7/dl_assignment_7_common.py
+++ lab7/dl_assignment_7_common.py
@@ -1,5 +1,9 @@
 # This is a file where you should put your own functions
 
+import torch
+import torch.nn
+
+
 # -----------------------------------------------------------------------------
 # Datasets
 # -----------------------------------------------------------------------------
@@ -12,14 +16,153 @@
 
 # TODO: Define network architectures here
 
+def create_lenet(image_size=28):
+    return torch.nn.Sequential(
+        torch.nn.Linear(28**2, 300),
+        torch.nn.ReLU(),
+        torch.nn.Linear(300, 100),
+        torch.nn.ReLU(),
+        torch.nn.Linear(100, 10)
+    )
+
+def create_conv_2(image_size=32):
+    return torch.nn.Sequential(
+        torch.nn.Conv2d(1, 64, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.Conv2d(64, 64, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
+
+        torch.nn.Flatten(),
+        torch.nn.Linear(int((image_size/2)**2 * 64), 256),
+        torch.nn.ReLU(),
+        torch.nn.Linear(256, 256),
+        torch.nn.ReLU(),
+        torch.nn.Linear(256, 10)
+    )
+
+def create_conv_4(image_size=32):
+    return torch.nn.Sequential(
+        torch.nn.Conv2d(1, 64, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.Conv2d(64, 64, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
+
+        torch.nn.Conv2d(64, 128, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.Conv2d(128, 128, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
+
+        torch.nn.Flatten(),
+        torch.nn.Linear(int((image_size/4)**2 * 128), 256),
+        torch.nn.ReLU(),
+        torch.nn.Linear(256, 256),
+        torch.nn.ReLU(),
+        torch.nn.Linear(256, 10)
+    )
+
+
+def create_conv_6(image_size=32):
+    return torch.nn.Sequential(
+        torch.nn.Conv2d(1, 64, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.Conv2d(64, 64, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
+
+        torch.nn.Conv2d(64, 128, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.Conv2d(128, 128, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
+
+        torch.nn.Conv2d(128, 256, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.Conv2d(256, 256, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
+
+        torch.nn.Flatten(),
+        torch.nn.LazyLinear(256),
+        torch.nn.ReLU(),
+        # torch.nn.Linear(int((image_size/8)**2 * 256), 256),
+        # torch.nn.ReLU(),
+        torch.nn.Linear(256, 256),
+        torch.nn.ReLU(),
+        torch.nn.Linear(256, 10)
+    )
+
+class ResidualBlock(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, strides=1, padding='same'):
+        super().__init__()
+        self.conv1 = torch.nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=strides, padding=1)
+        self.conv2 = torch.nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, stride=1, padding=padding)
+
+    def forward(self, X):
+        Y = torch.nn.functional.relu(self.conv1(X))
+        Y = self.conv2(Y)
+        Y += X
+        return torch.nn.functional.relu(Y)
+
+def create_resnet_18(image_size=32):
+    return torch.nn.Sequential(
+        torch.nn.Conv2d(1, 16, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+
+        ResidualBlock(16, 16),
+        ResidualBlock(16, 16),
+        ResidualBlock(16, 16),
+
+        ResidualBlock(16, 32, strides=2),
+        ResidualBlock(32, 32),
+        ResidualBlock(32, 32),
+
+        ResidualBlock(32, 64, strides=2),
+        ResidualBlock(64, 64),
+        ResidualBlock(64, 64),
+
+        torch.nn.AvgPool2d(kernel_size=(2, 2), stride=(2, 2)),
+        torch.nn.Linear(int((image_size/8)**2 * 64), 10),
+    )
+
+def create_vgg_19(image_size=32):
+    return torch.nn.Sequential(
+        torch.nn.Conv2d(1, 64, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.Conv2d(64, 64, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
+
+        torch.nn.Conv2d(64, 128, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.Conv2d(128, 128, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
+
+        torch.nn.Conv2d(128, 256, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.Conv2d(256, 256, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.Conv2d(256, 256, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.Conv2d(256, 256, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
+
+        torch.nn.Conv2d(256, 512, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.Conv2d(512, 512, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.Conv2d(512, 512, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.Conv2d(512, 512, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
+
+        torch.nn.Conv2d(512, 512, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.Conv2d(512, 512, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.Conv2d(512, 512, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+        torch.nn.Conv2d(512, 512, kernel_size=(3, 3), padding='same'), torch.nn.ReLU(),
+
+        torch.nn.AvgPool2d(kernel_size=(2, 2), stride=(2, 2)),
+        torch.nn.Linear(int((image_size/32)**2 * 64), 10),
+    )
+
 def create_network(arch, **kwargs):
     # TODO: Change this function for the architectures you want to support
-    if arch == 'arch1':
-        return create_network_arch1(**kwargs)
-    elif arch == 'arch2':
-        return create_network_arch2(**kwargs)
+    if arch == 'lenet':
+        return create_lenet(**kwargs)
+    elif arch == 'conv2':
+        return create_conv_2(**kwargs)
+    elif arch == 'conv4':
+        return create_conv_4(**kwargs)
+    elif arch == 'conv6':
+        return create_conv_6(**kwargs)
+    elif arch == 'resnet18':
+        return create_resnet_18(**kwargs)
+    elif arch == 'vgg19':
+        return create_vgg_19(**kwargs)
     else:
         raise Exception(f"Unknown architecture: {arch}")
+
+
 
 # -----------------------------------------------------------------------------
 # Training and testing loops
@@ -33,4 +176,4 @@ def create_network(arch, **kwargs):
 
 # TODO: Put functions related to pruning here
 
-
+#%%
